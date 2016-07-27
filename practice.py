74dd980d9a70014948d192027a3e839de02e7ed6
haoliang <haoliang@douban.com>
2016-07-27 22:22:38 +0800

    update

--- practice.py
+++ practice.py
@@ -1,12 +1,15 @@
-'''
-given nxn 2D matrix, reverse the matrix
-eg.
-given: [[1,2],[3,4]]
-expected: [[3,1], [4,2]]
-'''
+# -*- coding:utf-8 -*-
 
 
 def convert(input_matrix):
+
+    '''
+    given nxn 2D matrix, reverse the matrix
+    eg.
+    given: [[1,2],[3,4]]
+    expected: [[3,1], [4,2]]
+    '''
+
     if not input_matrix:
         return
     elif len(input_matrix) == 1:
@@ -24,7 +27,30 @@ def convert(input_matrix):
     return output_array
 
 
+def partitionArray(nums):
+    '''
+    Partition an integers array into odd number first and even number second.
+    eg.
+    input [2, 3, 4, 5]
+    output [3, 5, 2, 4]
+    '''
+    '''
+    :param nums:
+    :return:
+    '''
+    evenList = []
+    oddList = []
+    for i in nums:
+        if i % 2 == 0:
+            evenList.append(i)
+        else:
+            oddList.append(i)
+    return sorted(oddList) + sorted(evenList)
+
+
 if __name__ == '__main__':
     print convert([[1, 2, 3], [4, 5, 6]])
     print convert([[1,2], [3,4]])
     print convert([])
+
+    print partitionArray([2, 3, 4, 5])
